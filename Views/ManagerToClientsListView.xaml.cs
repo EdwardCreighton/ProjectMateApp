7cbbc7cf9fb9a6e6ed4a3f199dfe3a0174646d74
Edward Creighton <eduard.chekukha@mail.ru>
2024-02-26 15:23:33 +0300

    Added List Views for all cases

---
+++ Views/ManagerToClientsListView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectMateApp.Views
+{
+    /// <summary>
+    /// Interaction logic for ManagerToClientsListView.xaml
+    /// </summary>
+    public partial class ManagerToClientsListView : UserControl
+    {
+        public ManagerToClientsListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/ManagersListView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectMateApp.Views
+{
+    /// <summary>
+    /// Interaction logic for ManagersListView.xaml
+    /// </summary>
+    public partial class ManagersListView : UserControl
+    {
+        public ManagersListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
