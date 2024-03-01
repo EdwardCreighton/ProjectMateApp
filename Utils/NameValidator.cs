b66c40ca52e9fd084264e0329e45c2e78d8902ab
Edward Creighton <eduard.chekukha@mail.ru>
2024-03-01 22:20:52 +0300

    NumbersRegex fix

--- Utils/NameValidator.cs
+++ Utils/NameValidator.cs
@@ -5,7 +5,7 @@ namespace ProjectMateApp.Utils
 {
     public static class NameValidator
     {
-        private static readonly Regex NumbersRegex = new Regex("[0-9.-]+");
+        private static readonly Regex NumbersRegex = new Regex("[0-9]+");
 
         public static void Validate(string name)
         {
