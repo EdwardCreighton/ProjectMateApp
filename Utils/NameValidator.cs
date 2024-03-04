b317747025b1583498e7d393e26880552473a6b7
Edward Creighton <eduard.chekukha@mail.ru>
2024-03-04 04:09:18 +0300

    NameValidator refactor

--- Utils/NameValidator.cs
+++ Utils/NameValidator.cs
@@ -14,5 +14,22 @@ namespace ProjectMateApp.Utils
             if (numbersMatches.Count > 0)
                 throw new NameContainsNumbersException();
         }
+
+        public static string JoinName(string fn, string sn, string ln)
+        {
+            fn.Trim();
+            sn.Trim();
+            ln?.Trim();
+
+            return $"{fn} {sn} {ln}";
+        }
+
+        public static void SeparateName(string name, out string fn, out string sn, out string ln)
+        {
+            string[] parts = name.Split(' ');
+            fn = parts[0];
+            sn = parts[1];
+            ln = parts[2];
+        }
     }
 }
